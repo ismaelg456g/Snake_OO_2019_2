efedcc6579dd084b45fd364123467b588422fac7
ismaelg456g <ismael-456@hotmail.com>
2019-10-26 16:22:39 -0300

    Inicializando repositório

---
+++ src/snake/Snake.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snake;
+
+/**
+ *
+ * @author hercules
+ */
+public class Snake {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TelaInicial telaInicio = new TelaInicial();
+        
+        telaInicio.main(args);
+    }
+    
+}
